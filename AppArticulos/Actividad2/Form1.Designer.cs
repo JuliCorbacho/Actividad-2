42c8fc7a3919dfc139d3e9cba34b1323e5083003
Julieta <julieta.corbacho@gmail.com>
2022-09-12 20:32:05 -0300

    Estructura de formulario

--- AppArticulos/Actividad2/Form1.Designer.cs
+++ AppArticulos/Actividad2/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Actividad2
 {
-    partial class Form1
+    partial class frmArticulos
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,13 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgvArticulos = new System.Windows.Forms.DataGridView();
+            this.pbxArticulo = new System.Windows.Forms.PictureBox();
+            this.btnDetalles = new System.Windows.Forms.Button();
+            this.btnAgregarArticulo = new System.Windows.Forms.Button();
+            this.btnEliminarArticulo = new System.Windows.Forms.Button();
+            this.btnModificarArticulo = new System.Windows.Forms.Button();
+            this.lbArticulos = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxArticulo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvArticulos
+            // 
+            this.dgvArticulos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvArticulos.Location = new System.Drawing.Point(12, 52);
+            this.dgvArticulos.Name = "dgvArticulos";
+            this.dgvArticulos.Size = new System.Drawing.Size(716, 386);
+            this.dgvArticulos.TabIndex = 0;
+            this.dgvArticulos.SelectionChanged += new System.EventHandler(this.dgvArticulos_SelectionChanged);
+            // 
+            // pbxArticulo
+            // 
+            this.pbxArticulo.Location = new System.Drawing.Point(744, 54);
+            this.pbxArticulo.Name = "pbxArticulo";
+            this.pbxArticulo.Size = new System.Drawing.Size(206, 186);
+            this.pbxArticulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxArticulo.TabIndex = 1;
+            this.pbxArticulo.TabStop = false;
+            // 
+            // btnDetalles
+            // 
+            this.btnDetalles.Location = new System.Drawing.Point(769, 246);
+            this.btnDetalles.Name = "btnDetalles";
+            this.btnDetalles.Size = new System.Drawing.Size(157, 28);
+            this.btnDetalles.TabIndex = 2;
+            this.btnDetalles.Text = "Ver detalles";
+            this.btnDetalles.UseVisualStyleBackColor = true;
+            this.btnDetalles.Click += new System.EventHandler(this.btnDetalles_Click);
+            // 
+            // btnAgregarArticulo
+            // 
+            this.btnAgregarArticulo.Location = new System.Drawing.Point(580, 23);
+            this.btnAgregarArticulo.Name = "btnAgregarArticulo";
+            this.btnAgregarArticulo.Size = new System.Drawing.Size(148, 23);
+            this.btnAgregarArticulo.TabIndex = 3;
+            this.btnAgregarArticulo.Text = "Agregar articulo";
+            this.btnAgregarArticulo.UseVisualStyleBackColor = true;
+            this.btnAgregarArticulo.Click += new System.EventHandler(this.btnAgregarArticulo_Click);
+            // 
+            // btnEliminarArticulo
+            // 
+            this.btnEliminarArticulo.Location = new System.Drawing.Point(734, 415);
+            this.btnEliminarArticulo.Name = "btnEliminarArticulo";
+            this.btnEliminarArticulo.Size = new System.Drawing.Size(148, 23);
+            this.btnEliminarArticulo.TabIndex = 4;
+            this.btnEliminarArticulo.Text = "Eliminar articulo";
+            this.btnEliminarArticulo.UseVisualStyleBackColor = true;
+            this.btnEliminarArticulo.Click += new System.EventHandler(this.btnEliminarArticulo_Click);
+            // 
+            // btnModificarArticulo
+            // 
+            this.btnModificarArticulo.Location = new System.Drawing.Point(734, 386);
+            this.btnModificarArticulo.Name = "btnModificarArticulo";
+            this.btnModificarArticulo.Size = new System.Drawing.Size(148, 23);
+            this.btnModificarArticulo.TabIndex = 5;
+            this.btnModificarArticulo.Text = "Modificar articulo";
+            this.btnModificarArticulo.UseVisualStyleBackColor = true;
+            this.btnModificarArticulo.Click += new System.EventHandler(this.btnModificarArticulo_Click);
+            // 
+            // lbArticulos
+            // 
+            this.lbArticulos.AutoSize = true;
+            this.lbArticulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbArticulos.Location = new System.Drawing.Point(12, 9);
+            this.lbArticulos.Name = "lbArticulos";
+            this.lbArticulos.Size = new System.Drawing.Size(119, 31);
+            this.lbArticulos.TabIndex = 6;
+            this.lbArticulos.Text = "Articulos";
+            // 
+            // frmArticulos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(958, 450);
+            this.Controls.Add(this.lbArticulos);
+            this.Controls.Add(this.btnModificarArticulo);
+            this.Controls.Add(this.btnEliminarArticulo);
+            this.Controls.Add(this.btnAgregarArticulo);
+            this.Controls.Add(this.btnDetalles);
+            this.Controls.Add(this.pbxArticulo);
+            this.Controls.Add(this.dgvArticulos);
+            this.Name = "frmArticulos";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmArticulos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvArticulos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxArticulo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvArticulos;
+        private System.Windows.Forms.PictureBox pbxArticulo;
+        private System.Windows.Forms.Button btnDetalles;
+        private System.Windows.Forms.Button btnAgregarArticulo;
+        private System.Windows.Forms.Button btnEliminarArticulo;
+        private System.Windows.Forms.Button btnModificarArticulo;
+        private System.Windows.Forms.Label lbArticulos;
     }
 }
 
--- AppArticulos/Actividad2/Form1.cs
+++ AppArticulos/Actividad2/Form1.cs
@@ -10,11 +10,61 @@ using System.Windows.Forms;
 
 namespace Actividad2
 {
-    public partial class Form1 : Form
+    public partial class frmArticulos : Form
     {
-        public Form1()
+        //private List<Articulo> listaArticulo;
+
+        public frmArticulos()
         {
             InitializeComponent();
         }
+
+        private void frmArticulos_Load(object sender, EventArgs e)
+        {
+            //ArticuloNegocio negocio = new ArticuloNegocio();
+            //listaArticulo = negocio.listar();
+            //dgvArticulos.DataSource = listaArticulo
+            //cargarImagen(listaArticulo[0].UrlImagen);
+        }
+
+        private void dgvArticulos_SelectionChanged(object sender, EventArgs e)
+        {
+            //Articulo itemSeleccionado = (Articulo) dgvArticulos.CurrentRow.DataBoundItem;
+            //cargarImagen(itemSeleccionado.UrlImagen);
+        }
+
+        private void cargarImagen(string image)
+        {
+            try
+            {
+                //pbxArticulo.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                //pbxArticulo.Load("https://us.123rf.com/450wm/mathier/mathier1905/mathier190500002/134557216-sin-imagen-en-miniatura-marcador-de-posici%C3%B3n-para-foros-blogs-y-sitios-web.jpg?ver=6");
+            }
+        }
+
+        private void btnDetalles_Click(object sender, EventArgs e)
+        {
+            //Ver detalles de articulo
+        }
+
+        private void btnAgregarArticulo_Click(object sender, EventArgs e)
+        {
+            //Agregar articulo
+        }
+
+        private void btnModificarArticulo_Click(object sender, EventArgs e)
+        {
+            //Modificar articulo
+        }
+
+        private void btnEliminarArticulo_Click(object sender, EventArgs e)
+        {
+            //Eliminar articulo
+        }
+
     }
 }
